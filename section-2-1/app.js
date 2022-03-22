bb1912b7d836f3177085aa9633f7fec2ac986c24
eduardo escobar <war_army_10@hotmail.com>
2022-03-21 21:57:35 -0600

    Chapter 28: Conditional Rendering

--- section-2-1/app.js
+++ section-2-1/app.js
@@ -1,8 +1,32 @@
-Vue.createApp({
-	data(){
-		return {
-			firstName: 'lalo'
-			lastName: 'another lalo'
-		}
-	}
+const vm = Vue.createApp({
+  data() {
+    return {
+      firstName: 'John',
+      lastName: 'Doe',
+      url: 'https://google.com',
+      raw_url: '<a href="https://google.com" target="_blank">Google</a>',
+      age: 20
+    }
+  },
+  methods: {
+    increment() {
+      this.age++
+    },
+    updateLastName(event) {
+      this.lastName = event.target.value
+    }
+  },
+  computed: {
+  	fullName() {
+  		console.log('full name computed property was called')
+  		return `${this.firstName} ${this.lastName.toUpperCase()}`
+  	}
+  },
+  watch: {
+  	age(newVal, oldVal){
+  		setTimeout(() => {
+  			this.age = 20
+  		}, 3000)
+  	}
+  }
 }).mount('#app')
