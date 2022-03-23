5e9fe1b51684deebdc3bfff8b21c2bcb9579be13
Eduardo Escobar <jose.escobar@flex.com>
2022-03-22 18:24:44 -0600

    Chapter 42: The Vue Compiler

--- section-4-1-starter/app.js
+++ section-4-1-starter/app.js
@@ -1,33 +1,48 @@
 let vm = Vue.createApp({
-  data() {
-    return {
-      message: "Hello world!"
+    data() {
+        return {
+            message: "Hello world!"
+        }
+    },
+    template: '{{ message }}',
+    beforeCreate() {
+        console.log('beforeCreate() function called!', this.message)
+    },
+    created() {
+        console.log('created() function called!', this.message)
+    },
+    beforeMount() {
+        console.log('beforeMount() function called!', this.$el)
+    },
+    mounted() {
+        console.log('mounted() function called!', this.$el)
+    },
+    beforeUpdate() {
+        console.log('beforeUpdate() function called!')
+    },
+    updated() {
+        console.log('updated() function called!')
+    },
+    beforeUnmount() {
+        console.log('beforeUnmount() function called!')
+    },
+    unmounted() {
+        console.log('unmounted() function called!')
     }
-  },
-  beforeCreate() {
-    console.log('beforeCreate() function called!', this.message)
-  },
-  created() {
-    console.log('created() function called!', this.message)
-  },
-  beforeMount() {
-    console.log('beforeMount() function called!', this.$el)
-  },
-  mounted() {
-    console.log('mounted() function called!', this.$el)
-  },
-  beforeUpdate() {
-    console.log('beforeUpdate() function called!')
-  },
-  updated() {
-    console.log('updated() function called!')
-  },
-  beforeUnmount(){
-    console.log('beforeUnmount() function called!')
-  },
-  unmounted(){
-    console.log('unmounted() function called!')
-  }
 })
 
 vm.mount('#app')
+
+let vm2 = Vue.createApp({
+  data() {
+    return {
+      message: 'Hello World'
+    }
+  },
+  render() {
+    return Vue.h(
+        'h1',
+        this.message
+    )
+  }
+}).mount('#app2')
